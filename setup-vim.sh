4236df609672e011b34be4319b8ecf82568543cb
Samuel A. Winchenbach <swinchen@gmail.com>
2014-10-15 14:09:04 -0400

    Typo

--- setup-vim.sh
+++ setup-vim.sh
@@ -11,4 +11,4 @@ mkdir -p ~/.vim/autoload
 curl -fLo ~/.vim/autoload/plug.vim \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-vim -c "PluginUpdate"
+vim -c "PlugInstall"
