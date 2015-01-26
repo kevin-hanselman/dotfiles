d96d455636a8652afe930db47551fca055e1bb65
Kevin Hanselman <kevin.hanselman@gmail.com>
2015-01-26 12:00:45 -0500

    bspwm: renamed conky scripts

--- _config/bspwm/panel/scripts/toggle_conky.sh
+++ _config/bspwm/panel/scripts/toggle_conky.sh
@@ -5,12 +5,15 @@
 
 pkill -x dmenu
 
-if [ $(pgrep -cf conky_$1) -gt 0 ]; then
-    if [ -n "$(xdotool search --onlyvisible --class conky_$1)" ]; then
+file="${1}.conkyrc"
+class="conky_$1"
+
+if [ $(pgrep -cf "$file") -gt 0 ]; then
+    if [ -n "$(xdotool search --onlyvisible --class $class)" ]; then
         xdotool search --class conky windowunmap %@
     else
         xdotool search --class conky windowunmap %@
-        xdotool search --class conky_$1 windowmap %1 windowraise
+        xdotool search --class $class windowmap %1 windowraise
     fi
 else
     source ~/.config/bspwm/panel/config
@@ -23,13 +26,13 @@ else
         # NOTE: top_middle alone doesn't work with multiple screens of different pixel widths
         pos=$(((mon_offset + (mon_width - screen_width) / 2) * -1 ))
         echo $pos
-        conky -x "$pos" -c ~/.config/bspwm/panel/conky/conky_$1
+        conky -x "$pos" -c ~/.config/bspwm/panel/conky/$file
     elif [ "$1" == 'weather' ]; then
         google-chrome-stable 'https://www.google.com/#q=weather' &
     else
         # right-aligned, use alignment top_right in conkyrc
         pos=$((screen_width - (mon_width + mon_offset - 14)))
         echo $pos
-        conky -x "$pos" -c ~/.config/bspwm/panel/conky/conky_$1
+        conky -x "$pos" -c ~/.config/bspwm/panel/conky/$file
     fi
 fi
