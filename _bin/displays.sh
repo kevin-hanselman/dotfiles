f2b30f850a7b2827721f8f08bd41fb2fe26841eb
Kevin Hanselman <kevin.hanselman@gmail.com>
2015-06-16 14:57:13 -0400

    new monitor

--- _bin/displays.sh
+++ _bin/displays.sh
@@ -3,31 +3,50 @@
 # cache xrandr output for faster access
 xrandr > /tmp/xrandr.txt
 
-if [ $(cat /tmp/xrandr.txt | grep DP-1 | awk '{ print $2 }') == 'connected' ]; then
+if [ $(cat /tmp/xrandr.txt | grep DP-5 | awk '{ print $2 }') == 'connected' ]; then
     echo 'Found Display Port(s)'
-    xrandr --output LVDS-1 --mode 1920x1080 \
-           --output DP-1 --auto --right-of LVDS-1 --primary --rotate left \
-           --output DP-3 --auto --right-of DP-1 --rotate normal \
-           --output VGA-1 --off
-elif [ $(cat /tmp/xrandr.txt | grep VGA-1 | awk '{ print $2 }') == 'connected' ]; then
+#    xrandr --output LVDS-0 --mode 1920x1080 \
+#           --output DP-5 --mode 2560x1440 --primary --left-of LVDS-0 \
+#           --output VGA-0 --off \
+#           --output DP-0 --off \
+#           --output DP-1 --off \
+#           --output DP-2 --off \
+#           --output DP-3 --off \
+#           --output DP-4 --off
+
+elif [ $(cat /tmp/xrandr.txt | grep VGA-0 | awk '{ print $2 }') == 'connected' ]; then
     echo 'Found VGA'
-    xrandr --output VGA-1 --auto \
-           --output LVDS-1 --same-as VGA-1 --primary \
+    xrandr --output VGA-0 --auto \
+           --output LVDS-0 --same-as VGA-0 --primary \
+           --output DP-0 --off \
            --output DP-1 --off \
-           --output DP-3 --off
+           --output DP-2 --off \
+           --output DP-3 --off \
+           --output DP-4 --off \
+           --output DP-5 --off
+
 elif [ $(cat /tmp/xrandr.txt | grep DP-3 | awk '{ print $2 }') == 'connected' ]; then
     echo 'Found HDMI'
     xrandr --output DP-3 --auto \
-           --output LVDS-1 --same-as DP-3 --primary \
+           --output LVDS-0 --same-as DP-3 --primary \
+           --output DP-0 --off \
            --output DP-1 --off \
-           --output VGA-1 --off
+           --output DP-2 --off \
+           --output DP-4 --off \
+           --output DP-5 --off \
+           --output VGA-0 --off
+
 else
     echo 'No external displays found'
     # explicitly turn off other outputs in case we're switching from multi-display
-    xrandr --output LVDS-1 --mode 1920x1080 --primary \
+    xrandr --output LVDS-0 --mode 1920x1080 --primary \
+           --output DP-0 --off \
            --output DP-1 --off \
+           --output DP-2 --off \
            --output DP-3 --off \
-           --output VGA-1 --off
+           --output DP-4 --off \
+           --output DP-5 --off \
+           --output VGA-0 --off
 fi
 
 # cache changes to xrandr
