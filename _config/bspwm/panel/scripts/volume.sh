9979059a2e0acb503bfcb91c29cfe00abb2638a3
Kevin Hanselman <kevin.hanselman@gmail.com>
2015-07-22 15:31:38 -0400

    handle errors better in volume.sh

--- _config/bspwm/panel/scripts/volume.sh
+++ _config/bspwm/panel/scripts/volume.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-vol=$(amixer sget Master,0 | grep -Po '\d+%|\b(?:on|off)\b' | tr -d '\n')
-n=$(echo $vol | cut -d% -f1)
-stat=$(echo $vol | cut -d% -f2)
+vol=$(amixer sget Master,0 | grep -Po '\d+%|\b(?:on|off)\b' | tr -d '\n') || exit 1
+n=$(echo $vol | cut -d% -f1) || exit 1
+stat=$(echo $vol | cut -d% -f2) || exit 1
 
-if [ "$stat" == off ]; then
-    echo 
+if [ "$stat" == 'off' ]; then
+    echo ''
 elif [ "$n" -gt 70 ]; then
-    echo 
+    echo ''
 elif [ "$n" -gt 20 ]; then
-    echo 
+    echo ''
 else
-    echo 
+    echo ''
 fi
