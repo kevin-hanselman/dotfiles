e1dd226a29d5aa5c8d5346841e31ef466a396f44
Kevin Hanselman <kevin.hanselman@gmail.com>
2016-01-07 14:43:00 -0500

    add required vim directories to dotfiles to eliminate the need to manually create them

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-mkdir -p ~/.vim/undo
-mkdir ~/.vim/backup
-mkdir ~/.vim/tmp
-mkdir ~/.vim/plugins
+set -euo pipefail
 
 ./dotfiles.sh _vim*
 
-mkdir -p ~/.vim/autoload
-curl -fLo ~/.vim/autoload/plug.vim \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 vim -c 'PlugInstall' -c 'qa!'
