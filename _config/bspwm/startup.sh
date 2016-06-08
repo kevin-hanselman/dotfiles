71a5dec44c2995fe205e22def9ba265651acc589
Kevin Hanselman <kevin.hanselman@gmail.com>
2016-06-08 15:05:22 -0400

    try out xfce4-panel

--- _config/bspwm/startup.sh
+++ _config/bspwm/startup.sh
@@ -2,17 +2,13 @@
 
 # bspwm startup programs
 
-# TODO: if these doesn't make sxhkd faster, put sxhkd and compton at the end?
 [ "$(pgrep -cx sxhkd)" -eq 0 ] && sxhkd &
 
 [ "$(pgrep -cx compton)" -eq 0 ] && compton &
 
-[ "$(pgrep -cx start_panel.sh)" -eq 0 ] && ~/.config/bspwm/panel/start_panel.sh &
+[ "$(pgrep -cx xfce4-panel)" -eq 0 ] && xfce4-panel -d &
 
-#if [ "$(pgrep -cf gnome-keyring-daemon)" -eq 0 ]; then
-    #eval $(gnome-keyring-daemon --start)
-    #export SSH_AUTH_SOCK
-#fi
+[ "$(pgrep -cx nm-applet)" -eq 0 ] && nm-applet &
 
 if [ "$(pgrep -cx xautolock)" -eq 0 ]; then
     xautolock -time 3 \
@@ -25,11 +21,4 @@ fi
 [ "$(pgrep -cf xfce4-volumed)" -eq 0 ] && xfce4-volumed
 
 [ "$(pgrep -cx dropbox)" -eq 0 ] && dropbox
-
-BSPWM_STATE='/tmp/bspwm-state.json'
-
-if [ -e "$BSPWM_STATE" ] ; then
-    bspc wm -l "$BSPWM_STATE"
-    rm "$BSPWM_STATE"
-fi
 
