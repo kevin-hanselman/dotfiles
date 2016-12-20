6c0b002494d90347bf062c5a9596c6fd74120517
Kevin Hanselman <kevin.hanselman@gmail.com>
2016-12-20 10:33:13 -0500

    remove xfce4-panel, add weather to polybar, misc cleanup

--- bspwm/.config/bspwm/bspc_setup_monitors.sh
+++ bspwm/.config/bspwm/bspc_setup_monitors.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 [ -f ~/.fehbg ] && source ~/.fehbg
-xrandr > /tmp/xrandr.txt
 
-top_left_mon=$(grep '\s*connected' /tmp/xrandr.txt | grep '+0+0' | awk '{print $1}')
 bspc config top_padding 0
-bspc config -m "$top_left_mon" top_padding 20
+bspc config -m primary top_padding 20
 
 for monitor in $(bspc query -M); do
     bspc monitor "$monitor" -d {1,2,3,4,5}
 done
+
+killall polybar
+MONITOR=$(xrandr | grep primary | awk '{print $1}') polybar top
---
+++ bspwm/.config/bspwm/weather.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -euo pipefail
+
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+apikey=$(cut -d' ' -f1 "$dir/weather.rc")
+zipcode=$(cut -d' ' -f2 "$dir/weather.rc")
+
+# worldweatheronline is limited to 250 API calls per day, or about one every 6 minutes; don't push it!
+weather=$(curl -s "https://api.worldweatheronline.com/free/v2/weather.ashx?key=$apikey&q=$zipcode&format=json&num_of_days=1" | \
+          jq -r '.data.current_condition[] | .temp_F, .weatherDesc[].value' | tr '\n' ' ')
+
+temp=$(echo "$weather" | cut -d' ' -f1)°F
+conditions=$(echo "$weather" | cut -d' ' -f2-)
+conditions=$(echo "$conditions" | xargs)
+
+echo "$temp $conditions"
+
+# jq '.data.current_condition[]' = 
+# {
+#     "cloudcover": "75",
+#     "FeelsLikeC": "25",
+#     "FeelsLikeF": "77",
+#     "humidity": "61",
+#     "observation_time": "07:16 PM",
+#     "precipMM": "1.3",
+#     "pressure": "1010",
+#     "temp_C": "23",
+#     "temp_F": "73",
+#     "visibility": "16",
+#     "weatherCode": "116",
+#     "weatherDesc": [
+#         {
+#             "value": "Partly Cloudy"
+#         }
+#     ],
+#     "weatherIconUrl": [
+#         {
+#             "value": "http://cdn.worldweatheronline.net/images/wsymbols01_png_64/wsymbol_0002_sunny_intervals.png"
+#         }
+#     ],
+#     "winddir16Point": "SE",
+#     "winddirDegree": "130",
+#     "windspeedKmph": "13",
+#     "windspeedMiles": "8"
+# }
