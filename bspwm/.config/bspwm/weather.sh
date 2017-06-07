3674f5fc566c230217a8f5a8a32f87c298ed4acc
Kevin Hanselman <kevin.hanselman@gmail.com>
2017-06-07 15:58:58 -0400

    change weather.sh to use apixu

--- bspwm/.config/bspwm/weather.sh
+++ bspwm/.config/bspwm/weather.sh
@@ -5,45 +5,53 @@ set -euo pipefail
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 apikey=$(cut -d' ' -f1 "$dir/weather.rc")
-#zipcode=$(cut -d' ' -f2 "$dir/weather.rc")
 zipcode=$(curl -s 'https://duckduckgo.com/?q=what+is+my+ip&ia=answer&format=json' | \
           jq .Answer | grep -Po '\b\d{5}\b' | tail -n1)
 
-# worldweatheronline is limited to 250 API calls per day, or about one every 6 minutes; don't push it!
-weather=$(curl -s "https://api.worldweatheronline.com/free/v2/weather.ashx?key=$apikey&q=$zipcode&format=json&num_of_days=1" | \
-          jq -r '.data.current_condition[] | .temp_F, .weatherDesc[].value' | tr '\n' ' ')
+response=$(curl -s "https://api.apixu.com/v1/current.json?key=${apikey}&q=${zipcode}" | \
+           jq .current)
 
-temp=$(echo "$weather" | cut -d' ' -f1)°F
-conditions=$(echo "$weather" | cut -d' ' -f2-)
-conditions=$(echo "$conditions" | xargs)
+temp="$(echo "$response" | jq -r '.feelslike_f')°F"
+conditions=$(echo "$response" | jq -r '.condition.text')
 
 echo "$temp $conditions"
 
-# jq '.data.current_condition[]' =
-# {
-#     "cloudcover": "75",
-#     "FeelsLikeC": "25",
-#     "FeelsLikeF": "77",
-#     "humidity": "61",
-#     "observation_time": "07:16 PM",
-#     "precipMM": "1.3",
-#     "pressure": "1010",
-#     "temp_C": "23",
-#     "temp_F": "73",
-#     "visibility": "16",
-#     "weatherCode": "116",
-#     "weatherDesc": [
-#         {
-#             "value": "Partly Cloudy"
-#         }
-#     ],
-#     "weatherIconUrl": [
-#         {
-#             "value": "http://cdn.worldweatheronline.net/images/wsymbols01_png_64/wsymbol_0002_sunny_intervals.png"
-#         }
-#     ],
-#     "winddir16Point": "SE",
-#     "winddirDegree": "130",
-#     "windspeedKmph": "13",
-#     "windspeedMiles": "8"
-# }
+# Example:
+#{
+#    "location": {
+#        "name": "Portland",
+#        "region": "Maine",
+#        "country": "USA",
+#        "lat": 43.66,
+#        "lon": -70.29,
+#        "tz_id": "America/New_York",
+#        "localtime_epoch": 1496437927,
+#        "localtime": "2017-06-02 17:12"
+#    },
+#    "current": {
+#        "last_updated_epoch": 1496437209,
+#        "last_updated": "2017-06-02 17:00",
+#        "temp_c": 20.0,
+#        "temp_f": 68.0,
+#        "is_day": 1,
+#        "condition": {
+#            "text": "Partly cloudy",
+#            "icon": "//cdn.apixu.com/weather/64x64/day/116.png",
+#            "code": 1003
+#        },
+#        "wind_mph": 10.5,
+#        "wind_kph": 16.9,
+#        "wind_degree": 270,
+#        "wind_dir": "W",
+#        "pressure_mb": 1010.0,
+#        "pressure_in": 30.3,
+#        "precip_mm": 0.4,
+#        "precip_in": 0.02,
+#        "humidity": 35,
+#        "cloud": 75,
+#        "feelslike_c": 20.0,
+#        "feelslike_f": 68.0,
+#        "vis_km": 16.0,
+#        "vis_miles": 9.0
+#    }
+#}
