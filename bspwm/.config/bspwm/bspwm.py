09b44708079468cfa2c8bee08f100c855ebf419c
Kevin Hanselman <kevin.hanselman@gmail.com>
2017-06-29 10:16:59 -0400

    add ipython RC file, clean-up bspwm.py

--- bspwm/.config/bspwm/bspwm.py
+++ bspwm/.config/bspwm/bspwm.py
@@ -1,22 +1,22 @@
 '''A module for interacting with the bspwm X window manager'''
-import subprocess
+from subprocess import Popen, PIPE, run
 import json
 
 
 def subscribe(*subscribe_args):
     '''Returns a generator for messages from a 'bspc subscribe' call.'''
-    proc = subprocess.Popen(['bspc', 'subscribe'] + list(subscribe_args),
-                            stdout=subprocess.PIPE)
+    proc = Popen(['bspc', 'subscribe'] + list(subscribe_args),
+                 stdout=PIPE)
     while proc.poll() is None:
         yield from proc.stdout
 
 
 def bspc(*bspc_args):
     '''Run the bspc command-line tool'''
-    return subprocess.run(['bspc'] + list(bspc_args))
+    return run(['bspc'] + list(bspc_args), stdout=PIPE)
 
 
 def get_state():
     '''Returns bspwm's global state JSON object'''
-    proc = subprocess.run(['bspc', 'wm', '-d'], stdout=subprocess.PIPE)
+    proc = bspc('wm', '-d')
     return json.loads(proc.stdout)
