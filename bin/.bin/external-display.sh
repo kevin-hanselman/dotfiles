e7c10b5dd8d28004b93010b9918002f25ceb89e5
Kevin Hanselman <kevin.hanselman@gmail.com>
2018-04-19 21:53:20 -0400

    rename displays.sh to auto-config-displays.sh, add external-display.sh

---
+++ bin/.bin/external-display.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -euo pipefail
+
+prog=$(basename "$0")
+
+fatal() {
+    echo "$prog: $1" >&2
+    exit 1
+}
+
+active_displays=$(xrandr | grep -P '\d+x\d+\+\d+\+' | awk '{print $1}' | sort)
+
+[ "$(echo "$active_displays" | wc -l)" -eq 1 ] || fatal 'more than one active display'
+
+connected_displays=$(xrandr | grep -P '\bconnected\b' | awk '{print $1}' | sort)
+inactive_displays=$(comm -23 <(echo "$connected_displays") <(echo "$active_displays"))
+
+[ "$(echo "$inactive_displays" | wc -l)" -eq 1 ] || fatal 'more than one inactive display'
+
+case "$1" in
+    right)
+        position='--right-of'
+        ;;
+    left)
+        position='--left-of'
+        ;;
+    mirror)
+        position='--same-as'
+        ;;
+    *)
+        fatal 'argument must be one of {left, right, mirror}'
+        ;;
+esac
+
+set -x
+xrandr --output "$inactive_displays" --auto "$position" "$active_displays"
