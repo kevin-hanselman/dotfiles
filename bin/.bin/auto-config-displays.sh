7f84901229e0c912c8d797e051223e6b093a7691
Kevin Hanselman <kevin.hanselman@gmail.com>
2018-06-11 10:17:17 -0400

    add new monitor layout

--- bin/.bin/auto-config-displays.sh
+++ bin/.bin/auto-config-displays.sh
@@ -5,10 +5,10 @@ xrandr --dpi 96
 if [ "$(xrandr | grep DP-3 | awk '{ print $2 }')" == 'connected' ]; then
     echo 'Found Display Port(s)'
     xrandr --output VGA-0 --off \
-           --output LVDS-0 --mode 1920x1080 --pos 4000x1480 --rotate normal \
-           --output DP-5 --mode 2560x1440 --pos 2560x0 --rotate right \
+           --output LVDS-0 --mode 1920x1080 --pos 0x648 --rotate normal \
+           --output DP-5 --mode 1920x1080 --pos 4480x0 --rotate right \
            --output DP-4 --off \
-           --output DP-3 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
+           --output DP-3 --primary --mode 2560x1440 --pos 1920x288 --rotate normal \
            --output DP-2 --off \
            --output DP-1 --off \
            --output DP-0 --off
