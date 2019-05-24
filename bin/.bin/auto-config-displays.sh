5a42dab48a6e3a0551e54916cfea7683816b3774
Kevin Hanselman <kevin.hanselman@gmail.com>
2019-05-24 15:07:35 -0400

    simplify default auto-config-displays.sh, cleanup polybar config

--- bin/.bin/auto-config-displays.sh
+++ bin/.bin/auto-config-displays.sh
@@ -1,29 +1,9 @@
 #!/bin/bash
-xrandr --dpi 96
 
-# explicitly turn off other outputs in case we're switching from multi-display
-if [ "$(xrandr | grep DP-3 | awk '{ print $2 }')" == 'connected' ]; then
-    echo 'Found Display Port(s)'
-    xrandr --output VGA-0 --off \
-           --output LVDS-0 --mode 1920x1080 --pos 0x648 --rotate normal \
-           --output DP-5 --mode 1920x1080 --pos 4480x0 --rotate right \
-           --output DP-4 --off \
-           --output DP-3 --primary --mode 2560x1440 --pos 1920x288 --rotate normal \
-           --output DP-2 --off \
-           --output DP-1 --off \
-           --output DP-0 --off
+connected_displays=$(xrandr | grep -E '\bconnected\b')
 
-else
-    echo 'No external displays found'
-    xrandr --output LVDS-0 --auto --primary \
-           --output DP-0 --off \
-           --output DP-1 --off \
-           --output DP-2 --off \
-           --output DP-3 --off \
-           --output DP-4 --off \
-           --output DP-5 --off \
-           --output VGA-0 --off
+# If we're only connected to one monitor, make that our primary monitor (for polybar, etc.)
+if [ "$(echo "$connected_displays" | wc -l)" -eq 1 ]; then
+    display=$(echo "$connected_displays" | awk '{print $1}')
+    xrandr --output "$display" --auto --primary
 fi
-
-# cache changes to xrandr
-xrandr > /tmp/xrandr.txt
