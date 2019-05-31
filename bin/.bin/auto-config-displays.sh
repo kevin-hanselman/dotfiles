8d68e2833a07d6e10178b28bac9a78206c78c496
Kevin Hanselman <kevin.hanselman@gmail.com>
2019-05-31 16:37:21 -0400

    add separate file for machine-specific monitor layout

--- bin/.bin/auto-config-displays.sh
+++ bin/.bin/auto-config-displays.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+set -euo pipefail
+
+# Run the machine-specific default layout if one exists.
+if [ -x ~/.screenlayout/default.sh ]; then
+    ~/.screenlayout/default.sh
+    exit 0
+fi
 
 connected_displays=$(xrandr | grep -E '\bconnected\b')
-
 # If we're only connected to one monitor, make that our primary monitor (for polybar, etc.)
 if [ "$(echo "$connected_displays" | wc -l)" -eq 1 ]; then
     display=$(echo "$connected_displays" | awk '{print $1}')
