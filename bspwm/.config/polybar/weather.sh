faa9215df4c1f719514ad8fa02167ed34c7b5396
Kevin Hanselman <kevin.hanselman@gmail.com>
2019-12-07 16:52:09 -0500

    update weather bar to use genuinetools/weather

--- bspwm/.config/polybar/weather.sh
+++ bspwm/.config/polybar/weather.sh
@@ -2,57 +2,16 @@
 
 set -euo pipefail
 
-dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+weather_executable=~/go/bin/weather
 
-apikey=$(cut -d' ' -f1 "$dir/weather.rc")
-zipcode=$(curl -s 'https://duckduckgo.com/?q=what+is+my+ip&ia=answer&format=json' | \
-          jq .Answer | grep -Po '\b\d{5}\b' | tail -n1)
+[ -x "$weather_executable" ] || go get github.com/genuinetools/weather
 
-response=$(curl --compressed -s "https://api.apixu.com/v1/current.json?key=${apikey}&q=${zipcode}")
+degrees='°F'
 
-city=$(echo "$response" | jq -r '.location.name')
-region=$(echo "$response" | jq -r '.location.region')
-temp="$(echo "$response" | jq -r '.current.feelslike_f')°F"
-conditions=$(echo "$response" | jq -r '.current.condition.text')
+weather_json=$("$weather_executable" --json)
+current_temp=$(echo "$weather_json" | jq -r '.currently.apparentTemperature' | xargs printf '%.*f' 1)
+low_temp=$(echo "$weather_json" | jq -r '.daily.data[0].apparentTemperatureMin' | xargs printf '%.*f' 1)
+high_temp=$(echo "$weather_json" | jq -r '.daily.data[0].apparentTemperatureMax' | xargs printf '%.*f' 1)
+summary=$(echo "$weather_json" | jq -r '.daily.data[0].summary')
 
-echo "$temp and ${conditions,,} in ${city}, $region"
-
-# Example:
-#{
-#    "location": {
-#        "name": "Portland",
-#        "region": "Maine",
-#        "country": "USA",
-#        "lat": 43.66,
-#        "lon": -70.29,
-#        "tz_id": "America/New_York",
-#        "localtime_epoch": 1496437927,
-#        "localtime": "2017-06-02 17:12"
-#    },
-#    "current": {
-#        "last_updated_epoch": 1496437209,
-#        "last_updated": "2017-06-02 17:00",
-#        "temp_c": 20.0,
-#        "temp_f": 68.0,
-#        "is_day": 1,
-#        "condition": {
-#            "text": "Partly cloudy",
-#            "icon": "//cdn.apixu.com/weather/64x64/day/116.png",
-#            "code": 1003
-#        },
-#        "wind_mph": 10.5,
-#        "wind_kph": 16.9,
-#        "wind_degree": 270,
-#        "wind_dir": "W",
-#        "pressure_mb": 1010.0,
-#        "pressure_in": 30.3,
-#        "precip_mm": 0.4,
-#        "precip_in": 0.02,
-#        "humidity": 35,
-#        "cloud": 75,
-#        "feelslike_c": 20.0,
-#        "feelslike_f": 68.0,
-#        "vis_km": 16.0,
-#        "vis_miles": 9.0
-#    }
-#}
+echo "$low_temp / $current_temp / $high_temp $degrees, ${summary,,}"
